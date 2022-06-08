89efde3c64e01c439cac4e47017527006db262f1
Maxwell Etombi <etombimaxwell@gmail.com>
2022-06-08 00:13:47 -0400

    changes made

--- src/Pages/FirstPage.js
+++ src/Pages/FirstPage.js
@@ -1,15 +1,29 @@
 import React from 'react'
 import styled from 'styled-components'
-import { BgStars } from '../Assets'
-
+import { BgStars, Astro } from '../Assets'
+import { TbRocket } from 'react-icons/tb'
+import { HiArrowRight } from 'react-icons/hi'
+import { Routes, Route, Link } from 'react-router-dom'
+// HiArrowRight
+// TbRocket
 const FirstPage = () => {
   return (
     <Wrapper>
       {/* <img src='../Assets/DOJA FIRST.jpg' alt="" /> */}
       <div className='first-page-section'>
-        <img src={BgStars} alt='' />
-        <h3>Doja World</h3>
-        {/* <div className='image-bg'></div> */}
+        <img src={BgStars} alt='stars' className='bg-img' />
+        <img src={Astro} alt='Astronaunt' className='img-man' />
+        <div className='first-page-details'>
+          <span>Hi,</span>
+          <span>Welcome to</span>
+          <span>
+            <h3>Doja World </h3>
+            <TbRocket className='rocket' />
+          </span>
+        </div>
+        <div className='nav-container'>
+          <HiArrowRight />
+        </div>
       </div>
     </Wrapper>
   )
@@ -22,22 +36,68 @@ const Wrapper = styled.section`
     display: flex;
     flex-direction: column;
     align-items: center;
+    justify-content: center;
     font-family: 'Audiowide', cursive;
     background: var(--bg-main);
     color: var(--white-color);
     padding: 2rem;
   }
 
-  img {
+  .bg-img {
     width: 100%;
     height: 100%;
     position: absolute;
     top: 0;
   }
 
-  //   .image-bg {
-  //     background: url(${BgStars});
-  //   }
+  .img-man {
+    height: 100px;
+    width: 200px;
+    object-fit: cover;
+  }
+
+  .first-page-details {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 3rem;
+    // background: rgb(180, 113, 226);
+    // background: linear-gradient(
+    //   90deg,
+    //   rgba(180, 113, 226, 1) 0%,
+    //   rgba(128, 193, 213, 1) 61%
+    // );
+  }
+
+  span {
+    margin: 0.3rem;
+    font-size: 2em;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+
+  .rocket {
+    background: -webkit-linear-gradient(#eee, #333);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+
+  .nav-container {
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+    display: grid;
+    place-content: center;
+    background: rgb(180, 113, 226);
+    background: linear-gradient(
+      90deg,
+      rgba(180, 113, 226, 1) 0%,
+      rgba(128, 193, 213, 1) 61%
+    );
+    font-size: 1.3em;
+    cursor: pointer;
+    user-select: none;
+  }
 `
 
 export default FirstPage
