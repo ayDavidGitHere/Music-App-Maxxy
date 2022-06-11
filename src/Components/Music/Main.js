0f386ae147dbd7bf7a4c1ce7e8b44d2005cc9437
Ayoade David <adewoleayoade057@gmail.com>
2022-06-11 23:14:49 +0100

    Upt

--- src/Components/Music/Main.js
+++ src/Components/Music/Main.js
@@ -16,7 +16,7 @@ import { togglePlay, setSong } from '../../Features/eventReducer'
 // IoIosArrowUp
 const Main = () => {
   const { musicList } = useSelector((store) => store.effectSlice)
-  const { songIsPlaying, currentSong, currentAudio } = useSelector(
+  let { songIsPlaying, currentSong, currentAudio } = useSelector(
     (store) => store.eventSlice
   )
 
