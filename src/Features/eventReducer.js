05c5ebdf44cc3b5454f9d236e799d485e4853f00
Ayoade David <adewoleayoade057@gmail.com>
2022-06-12 05:31:01 +0100

    Update

--- src/Features/eventReducer.js
+++ src/Features/eventReducer.js
@@ -4,7 +4,7 @@ import { useLocation } from 'react-router-dom'
 const initialState = {
   toNextPage: true,
   currentSong: '',
-  currentAudio: null,
+  currentAudio: '',
   songIsPlaying: false,
 }
 
